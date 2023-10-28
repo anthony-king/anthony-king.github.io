9c7841420e033bd7e4f45077a462116e9786194d
Anthony King <anthonyking@gmail.com>
2023-10-28 13:30:53 -0400

    Add basic response for index route

--- myApp.js
+++ myApp.js
@@ -1,41 +1,14 @@
-let express = require('express');
+let express = require("express");
 let app = express();
 
-console.log("Hello World")
+function getIndex(req, res) {
+  res.send("Hello Express");
+}
+// function getAdaAnimation(req, res) {
+//   res.send("/ada-animation.html");
+// }
 
+// app.get("/ada-animation", getAdaAnimation);
+app.get("/", getIndex);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
